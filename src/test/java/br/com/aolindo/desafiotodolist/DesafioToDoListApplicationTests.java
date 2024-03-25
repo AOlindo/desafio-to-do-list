1733164072b587be0395776857753ba4ed4dab95
Adriana Olindo <adriana-olindo18@hotmail.com>
2024-03-25 15:37:38 -0300

    Projeto to do list criado

---
+++ src/test/java/br/com/aolindo/desafiotodolist/DesafioToDoListApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.aolindo.desafiotodolist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioToDoListApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
